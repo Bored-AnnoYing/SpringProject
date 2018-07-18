0cf0e52114e7cb0cb1050fcbbcb4c42f74bb74af
henfanren <henfanren_zz@163.com>
2018-07-18 11:13:29 +0800

    spring初始环境搭建

---
+++ src/main/java/com/henfanren/bean/HellowWorld.java
@@ -0,0 +1,12 @@
+package com.henfanren.bean;
+
+/**
+ * @ProjectName: SpringProject
+ * @ClassName: HellowWorld
+ * @Description:
+ * @Author: zhanzhao
+ * @Date: Created in 2018/7/18 11:11
+ * @Modified By:
+ */
+public class HellowWorld {
+}
---
+++ src/main/java/com/henfanren/main/MainApp.java
@@ -0,0 +1,12 @@
+package com.henfanren.main;
+
+/**
+ * @ProjectName: SpringProject
+ * @ClassName: MainApp
+ * @Description:
+ * @Author: zhanzhao
+ * @Date: Created in 2018/7/18 11:11
+ * @Modified By:
+ */
+public class MainApp {
+}
