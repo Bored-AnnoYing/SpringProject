c3702ca16c017344434476d59aa0f8c8f3ca34e4
henfanren <henfanren_zz@163.com>
2018-07-20 15:25:08 +0800

    spring @Required注解使用

---
+++ src/main/java/com/henfanren/bean/Student.java
@@ -0,0 +1,35 @@
+package com.henfanren.bean;
+
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * @ProjectName: SpringProject
+ * @ClassName: Student
+ * @Description:
+ * @Author: zhanzhao
+ * @Date: Created in 2018/7/20 15:00
+ * @Modified By:
+ */
+public class Student {
+
+    private Integer age;
+
+    private String name;
+
+    @Required
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    @Required
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
--- src/main/java/com/henfanren/main/MainApp.java
+++ src/main/java/com/henfanren/main/MainApp.java
@@ -1,6 +1,6 @@
 package com.henfanren.main;
 
-import com.henfanren.bean.TextEditor;
+import com.henfanren.bean.Student;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -29,15 +29,19 @@ public class MainApp {
         objB.getMessage2();
         objB.getMessage3();*/
 
-        TextEditor textEditor = (TextEditor) context.getBean("textEditor");
+        /*TextEditor textEditor = (TextEditor) context.getBean("textEditor");
         System.out.println(textEditor.getName());
-        textEditor.spellChecke();
+        textEditor.spellChecke();*/
 
         /*JavaCollection javaCollection = (JavaCollection) context.getBean("javaCollcetion");
         javaCollection.getAddressList();
         javaCollection.getAddressSet();
         javaCollection.getAddressMap();
         javaCollection.getAddressProp();*/
+
+        Student student = (Student) context.getBean("student");
+        System.out.println("Name : " + student.getName() );
+        System.out.println("Age : " + student.getAge() );
 
     }
 
